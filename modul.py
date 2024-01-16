a625be86cf2c5a0393e8e399ef6d53c3ddcc737c
msi <meibrohimov.@gmail.com>
2024-01-16 10:29:12 +0500

    # add var

--- modul.py
+++ modul.py
@@ -1,9 +1,19 @@
-
 name: list = []
 
 
+# modul func
 def topic(data: list) -> list:
     for x in data:
         if x[0] == "A":
             name.append(x)
     return name
+
+
+# modul var
+User: dict = {
+    "name": "Ali",
+    "username": "allnc",
+    "password": "afkshg55",
+    "email": "all@gmail.com"
+
+}
