588d95a7565874a76727a2169027b03d54d2aea5
msi <meibrohimov.@gmail.com>
2024-01-16 10:30:27 +0500

    # add var

--- lesson.py
+++ lesson.py
@@ -1,6 +1,11 @@
-from modul import topic
+from modul import topic, User
 
 student = ["Alice", "Bob", "Charlie", "Anjela", "Akbar"]
 
 s = topic(student)
 print(s)
+
+a = User
+print(a.values())
+print(a.keys())
+print(type(a))
